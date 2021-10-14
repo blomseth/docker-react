64fa676add43dbdd44f757e09ae169e86ea0bf79
blomseth <kristianblomseth@gmail.com>
2021-10-14 14:39:02 +0200

    Expose 80

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changens
+          Det her e jo helt vilt
         </p>
         <a
           className="App-link"
